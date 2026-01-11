10e0aac6b3146aac04456aa961060796af94a090
wind0009 <windsomdabire@gmail.com-github@example.com>
2026-01-11 03:57:24 +0000

    Correction de la connexion Supabase et nettoyage du code

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Identifiants récupérés de ton compte Supabase
-const supabaseUrl = 'https://pdntlbqibknuiunlevks.supabase.co';
-const supabaseAnonKey = 'sb_publishable_yC68N251zawFjfVjdqcF8A_fbjLT7im';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
